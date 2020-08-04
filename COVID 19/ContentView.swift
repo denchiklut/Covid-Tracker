cadb619c6a297f153611c2bdcf2dbe3c21dee2cb
daaleksandrov <daaleksandrov@domclick.ru>
2020-08-04 13:50:42 +0300

    Initial Commit

---
+++ COVID 19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID 19
+//
+//  Created by Александров Денис Александрович on 8/4/20.
+//  Copyright © 2020 Александров Денис Александрович. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
